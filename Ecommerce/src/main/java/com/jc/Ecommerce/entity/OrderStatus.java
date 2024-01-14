453373e0114c167eb9721e34cb74b86a2730aed2
root <root@DESKTOP-3NE4O1U>
2024-01-14 16:49:40 -0300

    Criação entidade Produc e Category.

---
+++ Ecommerce/src/main/java/com/jc/Ecommerce/entity/OrderStatus.java
@@ -0,0 +1,12 @@
+package com.jc.Ecommerce.entity;
+
+public enum OrderStatus {
+	
+	WAITING_PAYMENT,
+	PAID,
+	SHIPPED,
+	DELIVERED,
+	CANCELED;
+	
+
+}
---
+++ Ecommerce/src/main/java/com/jc/Ecommerce/entity/Product.java
@@ -0,0 +1,83 @@
+package com.jc.Ecommerce.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+
+	@Column(columnDefinition = "TEXT", length = 120)
+	private String description;
+	private Double price;
+	private String String;
+
+	@ManyToAny
+	@JoinTable(name = "tb_product_category", 
+	joinColumns = @JoinColumn(name = "product_id"), 
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories = new HashSet<>();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getString() {
+		return String;
+	}
+
+	public void setString(String string) {
+		String = string;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+	
+
+
+}
